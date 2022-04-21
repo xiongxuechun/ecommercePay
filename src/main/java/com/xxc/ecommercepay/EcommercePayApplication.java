0a2c0659b741bbfbccc6ec725b484ec617cad699
xiongxuechun <15293191747@163.com>
2022-04-21 17:01:45 +0800

    电商支付系统

---
+++ src/main/java/com/xxc/ecommercepay/EcommercePayApplication.java
@@ -0,0 +1,13 @@
+package com.xxc.ecommercepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xxc/ecommercepay/EcommercePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxc.ecommercepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
